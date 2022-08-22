873495122b1cf07ed629327db5256087c63e3708
Eunji Kang <etothej@naver.com>
2022-08-22 21:30:55 +0900

    get own nweets

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,13 +1,40 @@
 import { signOut } from "firebase/auth";
+import {
+  collection,
+  getDoc,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { authService } from "../fbase";
+import { dbService } from "../fbase";
 
-export default function Profile() {
+export default function Profile({ userObj }) {
   const navigate = useNavigate();
   const onLogOut = () => {
     signOut(authService);
     navigate("/");
   };
+
+  const getMyNweets = async () => {
+    const q = query(
+      collection(dbService, "nweets"),
+      where("creatorId", "==", userObj.uid),
+      orderBy("createdAt", "desc")
+    );
+
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+      console.log(doc.data());
+    });
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
 
   return (
     <>
